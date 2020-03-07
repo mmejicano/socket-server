9723552dca5870a8c1d39be824d77561a98ebccb
Darko <mejicanomarvin@gmail.com>
2020-03-07 11:54:10 -0600

    Primer commit rest server

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socket-Server
+
+
+Reconstruir modulos de node
+```
+npm install
+```
+Generar folder dist
+```
+tsc -w
+```
+
+```
+nodemon dist/
+```
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Server from "./classes/server";
+import router from "./routes/router";
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+// Body Parser
+server.app.use( bodyParser.urlencoded({extended: true}));
+server.app.use( bodyParser.json())
+
+//configuracion de CORS
+server.app.use( cors({origin: true, credentials: true}))
+
+//configuracion de rutas
+server.app.use('/', router)
+
+server.start( () => {
+    console.log(`Servidor corriendo en puerto ${server.port}`);
+    
+})
